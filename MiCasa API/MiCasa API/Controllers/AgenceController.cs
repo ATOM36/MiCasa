860b40aed3c6be0a548dd3dbf7dc5ed2b8d2db73
Thiombiano Abdoul Majid <tchiombianomajid@gmail.com>
2022-03-16 18:29:22 +0100

    Validation

--- MiCasa API/MiCasa API/Controllers/AgenceController.cs
+++ MiCasa API/MiCasa API/Controllers/AgenceController.cs
@@ -22,39 +22,23 @@ namespace MiCasa.Controllers
          *  De préférence la route et la fonction ont le même nom
          * ********************************************/
 
-        [HttpGet]
-        [Route("test")]
-        public object test()
-        {
-            var _connection = DbConnection.GetConnection();
-            string query = "SELECT * FROM \"Agence\"";
-            _connection.Open();
-
-            NpgsqlCommand cmd = new(query, _connection);
-            var reader = cmd.ExecuteReader();
-            DataTable ta = new();
-            ta.Load(reader);
-            reader.Close();
-
-            return new JsonResult(ta);
-        }
 
         [HttpGet]
         [Route("LogIn")]
-        public JsonResult LogIn([FromQuery] string username,[FromQuery] string password) => _bll.LogIn(username, password);
+        public JsonResult LogIn([FromQuery] string username, [FromQuery] string password) => DAL_Agence.LogIn(username, password);
 
         [HttpDelete]
         [Route("SupprimerCompte")]
-        public JsonResult SupprimerCompte([FromQuery] int agenceId) => _bll.SupprimerCompte(agenceId);
+        public JsonResult SupprimerCompte([FromQuery] int agenceId) => DAL_Agence.SupprimerCompte(agenceId);
 
 
         [HttpGet]
         [Route("GetAgence")]
         public JsonResult GetAgence([FromQuery] int startIndex, [FromQuery] int stopIndex) =>
-            _bll.GetAgence(startIndex, stopIndex);
+           DAL_Agence.GetAgence(startIndex, stopIndex);
 
         [HttpPut]
         [Route("BloquerCompteAgence")]
-        public JsonResult BloquerCompteAgence([FromHeader] int agenceId) => _bll.BloquerCompteAgence(agenceId);
+        public JsonResult BloquerCompteAgence([FromHeader] int agenceId) => DAL_Agence.BloquerCompteAgence(agenceId);
     }
 }
