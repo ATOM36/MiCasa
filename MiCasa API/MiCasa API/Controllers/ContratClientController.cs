e3b4810215869ad7672d2b7d8bb27234c1316007
Thiombiano Abdoul Majid <tchiombianomajid@gmail.com>
2022-05-10 11:04:10 +0100

    Fonctionnalités de contrat client

---
+++ MiCasa API/MiCasa API/Controllers/ContratClientController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+
+namespace MiCasa.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+public class ContratClientController : ControllerBase
+{
+    private readonly IContratClient? _bll;
+
+    public ContratClientController(IContratClient bll)
+    {
+        _bll = bll;
+    }
+
+    [Route("CreerContrat")]
+    [Authorize]
+    [HttpPost]
+    public async Task<JsonResult> CreerContrat([FromQuery] int clientId, [FromQuery] int agenceId) =>
+        new(await _bll!.CreerContrat(clientId, agenceId));
+
+
+    [Route("AnnulerContrat")]
+    [Authorize]
+    [HttpPatch]
+    public async Task<JsonResult> AnnulerContrat([FromQuery] int contratId) => new(await _bll!.AnnulerContrat(contratId));
+
+    [Route("Get")]
+    [Authorize]
+    [HttpGet]
+    public async Task<JsonResult> Get([FromQuery] int contratId) => new(await _bll!.Get(contratId));
+
+
+    [Route("GetRange")]
+    [Authorize]
+    [HttpGet]
+    public async Task<JsonResult> Get([FromQuery] int startIndex, [FromQuery] int stopIndex) =>
+        new(await _bll!.Get(startIndex, stopIndex));
+}
---
+++ MiCasa API/MiCasa API/Models/Dal/Dal_ContratClient.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MiCasa.Models.Dal;
+
+public class Dal_ContratClient
+{
+    private readonly AppDbContext? _context;
+
+    public Dal_ContratClient(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<Message> AnnulerContrat(int contratId)
+    {
+        try
+        {
+            ContratClient? contract = await _context!.ContratClients.FindAsync(contratId);
+
+            if (contract is not null)
+            {
+                contract.IsActive = 0;
+                await _context.SaveChangesAsync();
+
+                return new("Contrat annulé avec succès", true);
+            }
+            else
+                return new("Contrat introuvable", false);
+        }
+        catch (Exception e)
+        {
+            return new(e.Message, false);
+        }
+    }
+    public async Task<Message> CreerContrat(int clientId, int agenceId)
+    {
+        try
+        {
+            ContratClient contrat = new() {
+                AgenceId = agenceId,
+                ClientId = clientId,
+                CreationDate = DateTime.Today.ToShortDateString(),
+                IsActive = 1
+            };
+
+            await _context!.ContratClients.AddAsync(contrat);
+            await _context.SaveChangesAsync();
+
+            return new("Contrat créé avec succès", true);
+        }
+        catch (Exception e)
+        {
+            return new(e.Message, false);
+        }
+    }
+
+    public async Task<QueryData<object>> Get(int contratId)
+    {
+        try
+        {
+            ContratClient? contrat = await _context!.ContratClients.FindAsync(contratId);
+
+            if (contrat is not null)
+                return new(contrat, true);
+            else
+                return new("Contrat introuvable", false);
+        }
+        catch (Exception e)
+        {
+            return new(e.Message, false);
+        }
+    }
+
+    public async Task<QueryData<object>> GetRange(int startIndex, int stopIndex)
+    {
+        try
+        {
+            IEnumerable<ContratClient>? result = await _context!.ContratClients
+                .Skip(startIndex)
+                .Take(stopIndex)
+                .OrderBy(e => e.ContratId)
+                .Distinct()
+                .ToListAsync();
+
+            if (result is not null)
+                return new(result, true);
+            else
+                return new("Aucun contrat trouvé", false);
+        }
+        catch (Exception e)
+        {
+            return new(e.Message, false);
+        }
+    }
+}
