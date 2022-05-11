bb8bd0541afe2917248265508374ebb58cec6dba
boubafatima <101665546+boubafatima@users.noreply.github.com>
2022-05-11 23:49:49 +0200

    Update DAL_Client.cs

--- MiCasa API/MiCasa API/Models/Dal/DAL_Client.cs
+++ MiCasa API/MiCasa API/Models/Dal/DAL_Client.cs
@@ -101,4 +101,100 @@ public class DAL_Client
             return new Message(e.Message, false);
         }
     }
+    
+     public async Task<Message> ModifierProfile(Client c) {
+
+        try{
+            context.Clients.Update(c);
+            await context.SaveChangesAsync();
+            return new Message ("votre profil est mis à jour", true);
+        }
+         catch (Exception e)
+        {
+            return new(e.Message, false);
+        }
+    }
+
+        public async Task<Message> DebloquerCompte(int clientId)
+    
+    {
+        try
+        {
+            using (_connection = DbConnection.GetConnection())
+            {
+                string sql = "UPDATE \"Comptes\" SET \"IsBlocked\" = 0 FROM \"Client\" WHERE " +
+                    " \"Comptes\".\"CompteId\" = \"Client\".\"CompteId\" AND \"Client\".\"ClientId\" = @Id";
+                await _connection.OpenAsync();
+                NpgsqlCommand command = new(sql, _connection);
+                command.Parameters.AddWithValue("@Id", clientId);
+                await command.ExecuteNonQueryAsync();
+                return new Message("le compte est débloqué", true);
+            }
+        }
+        catch (Exception e)
+        {
+            return new Message(e.Message, false);
+        }
+        finally
+        {
+            await _connection!.CloseAsync();
+        }
+    
+    }   
+
+     public async Task<Message> logout(int clientId)
+    {
+        try
+        {
+            using (_connection = DbConnection.GetConnection())
+            {
+                string sql = "UPDATE \"Comptes\" SET \"IsConnected\" = 0 FROM \"Client\" WHERE " +
+                    " \"Comptes\".\"CompteId\" = \"Client\".\"CompteId\" AND \"Client\".\"Client\" = @Id";
+                await _connection.OpenAsync();
+                NpgsqlCommand cmd = new(sql, _connection);
+                cmd.Parameters.AddWithValue("@Id", clientId);
+                await cmd.ExecuteNonQueryAsync();
+                return new("Aurevoir!", true);
+            }
+        }
+        catch (Exception e)
+        {
+            return new(e.Message, false);
+        }
+        finally
+        {
+            await _connection!.CloseAsync();
+        }
+    }
+
+     public async Task<QueryData<object>> LogIn(string username, string password)
+    {
+        try
+        {
+
+            Client? result = (from compte in context!.Comptes
+                              where compte.Username == username &&
+                              compte.Password == password
+                              join client in context!.Clients on
+                              compte.CompteId equals client.Compte.CompteId
+                              select new Client {
+                                  Compte = compte,
+                                  ClientId= client.ClientId,
+                                  CompteId= client.CompteId,
+                              }).SingleOrDefault();
+
+            result.Compte.Token = _authService!.GenerateToken(result.Compte.Username, result.Compte.Mail, "Client");
+            result.Compte.IsConnected = 1;
+            return new(result, true);
+        }
+
+        catch (Exception e)
+        {
+            return new(new Message(e.Message, false), false);
+        }
+        finally
+        {
+            await _context!.SaveChangesAsync();
+        }
+    }
 }
